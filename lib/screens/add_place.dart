a13eab054934ccd3778f303945a107cb8ea2ef22
creator <th8creator@gmail.com>
2025-03-17 21:37:51 +0300

    first commit

---
+++ lib/screens/add_place.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+
+import 'package:favorite_places/widgets/image_input.dart';
+import 'package:favorite_places/widgets/location_input.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:favorite_places/providers/user_places.dart';
+
+class AddPlaceScreen extends ConsumerStatefulWidget {
+  const AddPlaceScreen({super.key});
+
+  @override
+  ConsumerState<AddPlaceScreen> createState() {
+    return _AddPlaceScreenState();
+  }
+}
+
+class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+  File? _selectedImage;
+
+  void _savePlace() {
+    final enteredTitle = _titleController.text;
+
+    if (enteredTitle.isEmpty || _selectedImage == null) {
+      return;
+    }
+
+    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle, _selectedImage!);
+
+    Navigator.pop(context);
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Add ew Place')),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(labelText: 'Title'),
+              controller: _titleController,
+              style: TextStyle(color: Colors.white),
+            ),
+            const SizedBox(height: 16),
+            ImageInput(onPickImage: (image) => _selectedImage = image),
+            const SizedBox(height: 16),
+            LocationInput(),
+            const SizedBox(height: 16),
+
+            ElevatedButton.icon(
+              onPressed: _savePlace,
+              icon: const Icon(Icons.add),
+              label: const Text('Add Place'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({super.key});
+
+  @override
+  State<LocationInput> createState() {
+    return _LocationInputState();
+  }
+}
+
+class _LocationInputState extends State<LocationInput> {
+
+  var _isGettingLocation = false;
+
+  void _getCurrentLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    setState(() {
+      _isGettingLocation = true;
+    });
+
+    locationData = await location.getLocation();
+
+    setState(() {
+      _isGettingLocation = false;
+    });
+    print(locationData.latitude);
+    print(locationData.longitude);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget previewContent = Text(
+      'No location chosen',
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.white),
+    );
+
+    if (_isGettingLocation) {
+      previewContent = const CircularProgressIndicator();
+    }
+
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 2,
+              color: Theme.of(context).colorScheme.primary.withAlpha(50),
+            ),
+          ),
+          child: previewContent,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton.icon(
+              icon: Icon(Icons.location_on),
+              label: const Text('Get Current location'),
+              onPressed: _getCurrentLocation,
+            ),
+            TextButton.icon(
+              icon: Icon(Icons.map),
+              label: const Text('Select on Map'),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
